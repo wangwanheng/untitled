39ed18fed51d015eee42290cafef67c4f4ecf73c
Admin <1918627193@qq.com>
2023-03-18 20:42:20 +0800

    你好，世界

--- src/LoopTest6.java
+++ src/LoopTest6.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 public class LoopTest6 {
     public static void main(String[] args) {
         //需求：程序自动生成一个1~100之间的随机数字，使用程序实现猜出这个数字是多少
+        //扩展小需求：加一个保底机制，三次猜不中，直接提示猜中了
 
         //注意点
-        //生成随机数的代码int number = r.nextInt(100)+1;
+        //1、生成随机数的代码int number = r.nextInt(100)+1;
         //不能写在循环的里面，否则每一次都会产生一个新的随机数
 
+        //2、抽奖机制
+        //用电卷去抽水晶
+        //保底 360 次
+
+        //计数器
+       //用来统计已经猜了多少次
+        int count = 0;
         //分析
         //1、生成一个1~100之间的随机数字
         Random r =new Random();
@@ -22,6 +30,12 @@ public class LoopTest6 {
             //猜的数字太大了，提示  大了
             //猜的数字太小了，提示  小了
             //猜的数字一样，提示  猜中了
+            count++;
+            if (count == 3){
+                System.out.println("猜中了");
+                break;
+            }
+            //以上是扩展小要求
             if (guessNumber > number){
                 System.out.println("大了");
             } else if (guessNumber < number) {
