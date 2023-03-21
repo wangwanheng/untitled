d7d653cd24a5cfbac21050926ab3687d61e821d9
wangwanheng <1918627193@qq.com>
2023-03-21 11:10:26 +0800

    2023,3,21

--- src/Test1.java
+++ src/Test1.java
@@ -2,22 +2,48 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
-        //键盘录入一个三位数，获取其中的个位，十位，百位；
-        Scanner sc =new Scanner(System.in);
-        System.out.println("请输入一个三位数");
-        int number = sc.nextInt();
-        //获取个位，十位，百位
-        //公式：
-        //个位：  数字 % 10
-        //十位：  数字 /10 %10
-        //百位：  数字 /100 %10
-        //...
-        int ge =number % 10;
-        int shi = number /10 %10;
-        int bai = number /100 %10;
-        System.out.println(ge);
-        System.out.println(shi);
-        System.out.println(bai);
-        System.out.println("你好");
+      //机票价格按照淡季旺季、头等舱和经济舱收费、输入机票原价、月份和头等舱或经济舱。
+        // 按照如下规则计算机票价格：旺季（5~10月）头等舱9折，经济舱8.5折，淡季（11月到来年4月）头等舱七折，经济舱6.5折
+
+        //分析
+        //1、键盘录入机票原价、月份、头等舱或经济舱
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入机票的原价");
+        int ticket = sc.nextInt();
+        System.out.println("请输入当前月份");
+        int month = sc.nextInt();
+        System.out.println("请输入当前购买的舱位 0 头等舱  1 经济舱");
+        int seat = sc.nextInt();
+
+        //2、先判断该月份是旺季还是淡季
+        if (month >= 5 && month <= 10){
+            //旺季 //3、据徐判断当前机票是头等舱还是经济舱
+            if(seat ==0){
+                //头等舱
+                ticket = (int) (ticket * 0.9);
+            }else if (seat ==1){
+                //经济舱
+                ticket = (int) (ticket * 0.85);
+            }else{
+                System.out.println("没有这个舱位");
+            }
+        }else if((month >= 1 && month <=4 ) || (month >= 11 && month <= 12)){
+            //淡季  //3、据徐判断当前机票是头等舱还是经济舱
+            if(seat ==0){
+                //头等舱
+                ticket = (int) (ticket * 0.7);
+            }else if (seat ==1){
+                //经济舱
+                ticket = (int) (ticket * 0.65);
+            }else{
+                System.out.println("没有这个舱位");
+            }
+
+        }else{
+            //表示键盘录入的月份是一个非法数据
+            System.out.println("键盘录入的月份不合法");
+        }
+        System.out.println(ticket);
     }
+
 }
